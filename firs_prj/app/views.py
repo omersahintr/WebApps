daa177c93d471e5ccd32c65ea5934a3031d72050
Omer Sahin <bilgi@omersahin.com.tr>
2025-04-21 22:47:53 +0300

    404 error page has added.

--- firs_prj/app/views.py
+++ firs_prj/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 course_dict = {
     "python":"Welcome to Python Course",
@@ -13,8 +13,15 @@ def index(request):
     return HttpResponse("First Django Project Test OK.")
 
 
-def course(request, userurl): #user_url: user browser written url.
-    return HttpResponse(course_dict.get(userurl,"Page is Not Found - 404"))
+##def course(request, userurl): #user_url: user browser written url.
+    ##return HttpResponse(course_dict.get(userurl,"Page is Not Found - 404"))
 
 def multi_view(request,n1,n2):
     return HttpResponse(f"{n1}^{n2}={pow(n1,n2)}")
+
+def course(request,userurl):
+    try:
+        course = course_dict[userurl]
+        return HttpResponse(course)
+    except:
+        raise Http404("404 - This page is not found!")
