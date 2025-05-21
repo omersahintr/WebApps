0029ba2c67871deef15c8f81ad8cb3a497b1cff8
Omer Sahin <bilgi@omersahin.com.tr>
2025-05-21 14:39:03 +0300

    one test operation has done

--- Template/temp_app/views.py
+++ Template/temp_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Main Page")
