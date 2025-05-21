dc92e26c180651924573b8c9a5357f3540f5a54c
Omer Sahin <bilgi@omersahin.com.tr>
2025-05-21 19:59:08 +0300

    jinja syntax added

--- Template/temp_app/views.py
+++ Template/temp_app/views.py
@@ -7,6 +7,9 @@ def index(request):
     return render(request,"temp_app/first.html")
 
 def currency(request):
-    currency_dict = {"Dollar":38.8, "Euro":44.05, "Gold":3982.44, "BTC":4237678.26}
+    currency_dict = {"Dollar":[37.7,38.8], 
+                     "Euro":[43.55,44.05], 
+                     "Gold":[3890.05,3982.44], 
+                     "BTC":{"Ethereum":100590.84,"Doge":9.15,"Lite":3794.85}}
 
     return render(request,"temp_app/currency.html",context=currency_dict)
