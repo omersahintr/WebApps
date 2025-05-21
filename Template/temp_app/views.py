66e00b5cf169b5c03c54624fb4df9cb64f08f9e3
Omer Sahin <bilgi@omersahin.com.tr>
2025-05-21 18:37:28 +0300

    jinja syntax with html tags added

--- Template/temp_app/views.py
+++ Template/temp_app/views.py
@@ -5,3 +5,8 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     return render(request,"temp_app/first.html")
+
+def currency(request):
+    currency_dict = {"Dollar":38.8, "Euro":44.05, "Gold":3982.44, "BTC":4237678.26}
+
+    return render(request,"temp_app/currency.html",context=currency_dict)
