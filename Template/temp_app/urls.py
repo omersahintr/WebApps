bb8c6ab03468054b950ec4ff8fa6620ff80ecdcc
Omer Sahin <bilgi@omersahin.com.tr>
2025-05-25 11:34:59 +0300

    last commit added for filter jinja syntax

--- Template/temp_app/urls.py
+++ Template/temp_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "temp_app"
+
 urlpatterns = [
     path("",views.index,name="index"),
     path("currency/",views.currency,name="currency"),
